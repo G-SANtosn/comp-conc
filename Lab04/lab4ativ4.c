94d883432e8dc28150c9ce3548f461031137284f
G-SANtosn <75955483+G-SANtosn@users.noreply.github.com>
2021-01-13 23:10:37 -0300

    Add files via upload

---
+++ Lab04/lab4ativ4.c
@@ -0,0 +1,107 @@
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Módulo 2 - Laboratório 4 */
+/* Codigo: Controla a ordem de execução das threads em C */
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+
+int* matriz;
+int nthreads;
+int zeros;
+
+#define NTHREADS 4
+
+/* Variaveis globais */
+int x = 0;
+pthread_mutex_t x_mutex;
+pthread_cond_t x_cond;
+
+
+/* Thread A */
+void* A(void* t) {
+    printf("tudo bem?\n");
+
+    pthread_mutex_lock(&x_mutex);
+    x++;
+
+    ///se x for 2, as tarefas das threads A e B já acabaram
+    if (x == 2) {
+        pthread_cond_broadcast(&x_mutex);
+
+    }
+    pthread_mutex_unlock(&x_mutex);
+
+    pthread_exit(NULL);
+}
+
+/* Thread B */
+void* B(void* t) {
+    printf("bom dia!\n");
+
+    pthread_mutex_lock(&x_mutex);
+    x++;
+
+    //se x for 2, as tarefas das threads A e B já acabaram
+    if (x == 2) {
+        pthread_cond_broadcast(&x_mutex);
+
+    }
+    pthread_mutex_unlock(&x_mutex);
+
+    pthread_exit(NULL);
+}
+
+/* Thread C */
+void* C(void* t) {
+    //espera as tarefas das threads A e B acabarem
+    pthread_mutex_lock(&x_mutex);
+    if (x != 2) {
+        pthread_cond_wait(&x_cond, &x_mutex);
+    }
+    pthread_mutex_unlock(&x_mutex);
+    
+    printf("até mais!\n");
+    pthread_exit(NULL);
+}
+
+/* Thread D */
+void* D(void* t) {
+    //espera as tarefas das threads A e B acabarem
+    pthread_mutex_lock(&x_mutex);
+    if (x != 2) {
+        pthread_cond_wait(&x_cond, &x_mutex);
+    }
+    pthread_mutex_unlock(&x_mutex);
+
+    printf("boa tarde!\n");
+    pthread_exit(NULL);
+}
+
+//--funcao principal do programa
+int main(int argc, char* argv[]) {
+    int i;
+    pthread_t threads[NTHREADS];
+
+    /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
+    pthread_mutex_init(&x_mutex, NULL);
+    pthread_cond_init(&x_cond, NULL);
+
+    /* Cria as threads */
+    pthread_create(&threads[0], NULL, A, NULL);
+    pthread_create(&threads[1], NULL, B, NULL);
+    pthread_create(&threads[2], NULL, C, NULL);
+    pthread_create(&threads[3], NULL, D, NULL);
+
+    /* Espera todas as threads completarem */
+    for (i = 0; i < NTHREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("\nFIM\n");
+
+    /* Desaloca variaveis e termina */
+    pthread_mutex_destroy(&x_mutex);
+    pthread_cond_destroy(&x_cond);
+    return 0;
+}
