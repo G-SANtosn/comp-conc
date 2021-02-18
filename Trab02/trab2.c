9493f5d0f8491b708ffb905723d4c6b8f4821944
Gabriel Santos de Araujo do Nascimento <75955483+G-SANtosn@users.noreply.github.com>
2021-02-18 16:27:33 -0300

    Add files via upload

--- Trab02/trab2.c
+++ Trab02/trab2.c
@@ -34,29 +34,39 @@ tFila* fila;
 
 //--funcao que adiciona elementos na fila
 int push(tFila* fila, int low, int high) {
+    printf("Debug: Entrou no metodo push.\n");
     pthread_mutex_lock(&x_mutex);
-    
-    tElems* novo = malloc(sizeof(tElems));
+    printf("Debug: Teste.\n");
+
+    tElems* novo = (tElems*)malloc(sizeof(tElems));
     if (novo == NULL) {
         printf("ERRO--malloc\n");
         return 3;
     }
+    printf("Debug: Alocou elemento novo.\n");
 
     novo->low = low;
+    printf("Debug: novo->low = low (%d).\n", low);
     novo->high = high;
+    printf("Debug: novo->high = high (%high).\n", high);
     novo->next = NULL;
+    printf("Debug: novo->next = NULL.\n");
 
     if (fila->bottom != NULL) {
         fila->bottom->next = novo;
+        printf("Debug: Colocou.\n");
     }
     fila->bottom = novo;
+    printf("Debug: Colocou elemento no final da fila.\n");
 
     //primeiro elemento
     if (fila->top == NULL) {
         fila->top = novo;
+        printf("Debug: Colocou elemento no comeco da fila.\n");
     }
 
     free(novo);
+    printf("Debug: push(): Liberou espaco da variavel auxiliar.\n");
 
     pthread_mutex_unlock(&x_mutex);
 
@@ -112,7 +122,7 @@ void quicksort(int* array, int low, int high) {
     //void* arg
 
     //tElems* elementos = (tEles*)arg;
-    
+
     if (low < high) {
         int pi = partition(array, low, high);
 
@@ -132,14 +142,14 @@ void quicksort(int* array, int low, int high) {
 
 //--funcao executada pelas threads
 void* executaTarefa(void* arg) {
-    
+
     //tFila* fila = (tFila*)arg;
     tArgs* argumentos = (tArgs*)arg;
     printf("Thread %d iniciando.\n", argumentos->id);
 
     while (fila->top != NULL) {
         pthread_mutex_lock(&x_mutex);
-                
+
         tElems* elemento = pop(fila);
         printf("Thread %d pegou a parte de %d a %d do vetor.\n", argumentos->id, elemento->low, elemento->high);
         quicksort(vetor, elemento->low, elemento->high);
@@ -159,21 +169,20 @@ void* executaTarefa(void* arg) {
 //--funcao de inicializacao
 int inicializa(int tam) {
     vetor = (int*)malloc(sizeof(int) * tam);
-        
     if (vetor == NULL) {
         printf("ERRO--malloc\n");
         return 2;
     }
+    printf("Debug: Alocou vetor.\n");
 
     for (int i = 0; i < tam; i++) {
-        matriz[i] = (rand() % 100);
+        vetor[i] = (rand() % 100);
     }
-
-    push(fila, 0, tam - 1);
+    printf("Debug: Preencheu o vetor com valores aleatórios.\n");
 
     pthread_mutex_init(&x_mutex, NULL);
     pthread_cond_init(&x_cond, NULL);
-    
+
     return 0;
 }
 
@@ -181,8 +190,8 @@ int inicializa(int tam) {
 int imprimeVetor(int tam) {
     printf("Vetor:\n");
     for (int i = 0; i < tam; i++) {
-        printf("%d ", matriz[i]);
-        
+        printf("%d ", vetor[i]);
+
         if ((i + 1) % 10 == 0) {
             printf("\n");
         }
@@ -195,11 +204,11 @@ int imprimeVetor(int tam) {
 //--funcao principal do programa
 int main(int argc, char* argv[]) {
     int tam;            //tamanho do vetor
-    pthread_t *tid;      //identificadores das threads
+    pthread_t* tid;      //identificadores das threads
     //tFila* fila;
     tArgs* argumentos;  //identificadores locais das threads
     double inicio, fim, delta;
-        
+
     //Inicializacao
     GET_TIME(inicio);
     if (argc < 3) {
@@ -208,6 +217,8 @@ int main(int argc, char* argv[]) {
     }
     tam = atoi(argv[1]);
     nthreads = atoi(argv[2]);
+
+    printf("Debug: Começou.\n");
 
     if (nthreads > tam) {
         nthreads = tam;
@@ -228,19 +239,25 @@ int main(int argc, char* argv[]) {
         printf("ERRO--malloc\n");
         return 2;
     }
+    printf("Debug: Alocou a fila.\n");
     fila->top = NULL;
     fila->bottom = NULL;
+    printf("Debug: Elemento vazio na fila.\n");
+    push(fila, 0, tam - 1);
+
 
     tid = (pthread_t*)malloc(sizeof(pthread_t) * nthreads);
     if (tid == NULL) {
         printf("ERRO--malloc\n");
         return 2;
     }
+    printf("Debug: Alocou threads.\n");
     argumentos = (tArgs*)malloc(sizeof(tArgs) * nthreads);
     if (argumentos == NULL) {
         printf("ERRO--malloc\n");
         return 2;
     }
+    printf("Debug: Alocou argumentos.\n");
 
     for (int i = 0; i < nthreads; i++) {
         (argumentos + i)->id = i;
@@ -252,14 +269,14 @@ int main(int argc, char* argv[]) {
     }
 
     for (int i = 0; i < nthreads; i++) {
-        pthread_join(*(tid+i), NULL);
+        pthread_join(*(tid + i), NULL);
     }
     //Fim da alocação
     GET_TIME(fim);
     delta = fim - inicio;
     printf("Tempo de alocação: %lf\n", delta);
 
-    
+
     //Finalizacao
     /* Desaloca variaveis e termina */
     GET_TIME(inicio);
